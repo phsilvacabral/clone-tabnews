48676f8aab9b409553dad5ed9dc1053460d35d51
Pedro Cabral <126737207+phsilvacabral@users.noreply.github.com>
2026-01-14 17:33:33 +0000

    modificações feitas usando o prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Hello, world!</h1>
+  return <h1>Hello, world!</h1>;
 }
 
-export default Home
+function teste() {
+  console.log("teste");
+}
+
+export default Home;
